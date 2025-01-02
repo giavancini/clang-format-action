be979145b9ddfff843a2eb37255a02bee627b6be
giavancini <giovanea@unicamp.br>
2025-01-02 15:00:06 -0300

    First commit. Everything is a mess

---
+++ main.cpp
@@ -0,0 +1,19 @@
+       #include   <iostream>
+       #include <vector>
+
+void sum(int a,int b)
+{
+    int c=a+b;
+    std::cout<<c<<std::endl;
+}
+
+int main() {
+        std::cout << "Hello, World!" << std::endl;
+        #ifndef ZERO
+        std::vector<int>
+        v={1,2,3,4,5};
+        int* ptr = &v[0];
+        int& reallocf = v[0];
+        #endif
+    return 0;
+}
