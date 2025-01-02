bd55ee2869b84a699aa3303a36d079e2fccb1880
giavancini <giovanea@unicamp.br>
2025-01-02 19:08:29 -0300

    another modification in main to trigger action

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
         std::vector<int>
         v={1,2,3,4,5};
         int* ptr = &v[0];
-        int& reallocf = v[0];
+        int& reallocf = v[0]   ;
         #endif
     return 0;
 }
