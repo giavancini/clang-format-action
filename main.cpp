a898807050999b7621c4d775e3b0b782e3446536
giavancini <giovanea@unicamp.br>
2025-01-02 18:39:56 -0300

    try: unformatting main.cpp to test action

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
-#include <vector>
+       #include   <iostream>
+       #include <vector>
 
-void sum(int a, int b) {
-  int c = a + b;
-  std::cout << c << std::endl;
+void sum(int a,int b)
+{
+    int c=a+b;
+    std::cout<<c<<std::endl;
 }
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-#ifndef ZERO
-  std::vector<int> v = {1, 2, 3, 4, 5};
-  int *ptr = &v[0];
-  int &reallocf = v[0];
-#endif
-  return 0;
+        std::cout << "Hello, World!" << std::endl;
+        #ifndef ZERO
+        std::vector<int>
+        v={1,2,3,4,5};
+        int* ptr = &v[0];
+        int& reallocf = v[0];
+        #endif
+    return 0;
 }
