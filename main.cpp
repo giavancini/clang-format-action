4f2cb0a015f9c678829c08fc973ba294cccb9b6c
Francisco Orlandini <francisco.orlandini@gmail.com>
2025-01-07 11:20:20 -0300

    debugging action

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void sum(int a,int b)
 int main() {
         std::cout << "Hello, World!" << std::endl;
         #ifndef ZERO
-        std::vector<int>
+                      std::vector<int>
         v={1,2,3,4,5};
         int* ptr = &v[0];
         int& reallocf = v[0]   ;
